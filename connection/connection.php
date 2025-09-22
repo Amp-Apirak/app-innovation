8ba559a9cdd7a7eb8f1a925db7f1a067bc646aa3
Amp-Apirak <Apirak.ba@gmail.com>
2025-09-22 23:17:45 +0700

    Add

--- connection/connection.php
+++ connection/connection.php
@@ -1,6 +1,10 @@
 <?php 
-define('BASE_URL', '/app-innovation');
-define('BASE_APP', dirname(__DIR__));
+if (!defined('BASE_URL')) {
+    define('BASE_URL', '/app-innovation');
+}
+if (!defined('BASE_APP')) {
+    define('BASE_APP', dirname(__DIR__));
+}
 
       $conn = new mysqli('localhost','root','1234','db_pms'); //ประกาศตัวแปล $conn เก็บค่า การเชื่อมต่อ 
         if ($conn->connect_error) {  //ตรวจสอบเงื่อนไข ฐานข้อมูลเชื่อมต่อได้หรือไม่ หากไม่ให้แสดง error เป็นตัวเลข ออกมา
